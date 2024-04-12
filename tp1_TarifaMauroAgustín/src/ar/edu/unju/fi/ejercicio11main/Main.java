63b3490fada7646ef9bf141104da1b4225143589
DaSta <DaSta@DESKTOP-AB6GO6R>
2024-04-12 20:41:09 -0300

    Se agrega el punto 11 completo.

---
+++ tp1_TarifaMauroAgustín/src/ar/edu/unju/fi/ejercicio11main/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int j = 40, i = 2; j > 0; j--, i++ ) {
+			System.out.print(" "+ j * i+ " " );
+		}
+
+	}
+
+}
